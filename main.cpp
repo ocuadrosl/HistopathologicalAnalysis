f8e8b18e4f45bf1c991cfd356516d5275b915782
oscar <ocuadrosl@gmail.com>
2019-08-19 19:56:20 -0300

    main window

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <QApplication>
+
+#include "mainwindow.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    QApplication app (argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
